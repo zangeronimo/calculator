8ad2411d8f33e8b16b30899b31c60c5bf66e6823
Luciano Zangeronimo <zangeronimo@gmail.com>
2020-05-02 22:59:01 -0300

    End of the Calculator!

---
+++ src/components/Display.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const style = StyleSheet.create({
+    display: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        backgroundColor: 'rgb(0,0,0.6)',
+        alignItems: 'flex-end',
+    },
+    displayValue: {
+        fontSize: 60,
+        color: '#fff',
+    }
+})
+
+export default props =>
+    <View style={style.display}>
+        <Text style={style.displayValue}
+            numberOfLines={1}>{props.value}</Text>
+    </View>
